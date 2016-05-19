673b349d41dc59d7030be687c1ae67c94cba5d7c
Kyle MacDonald <macdonaldky@gmail.com>
2016-05-19 11:39:52 -0400

    add basic netlify hosting support to base tpl

--- init.js
+++ init.js
@@ -8,5 +8,22 @@ exports.configure = [
   }, {
     name: 'github_username',
     message: 'What is your github username?'
+  }, {
+    name: 'hosting',
+    message: 'Do you want to set-up hosting now? (via netlify)',
+    type: 'confirm',
+    default: false
   }
 ]
+
+exports.after = function(utils, config) {
+  if (config.hosting) {
+    console.log()
+    console.log('to complete hosting...')
+    console.log('- cd into your new project')
+    console.log('- npm install')
+    console.log('- create a new github repository & add origin')
+    console.log('- run `npm run netlify`')
+    console.log()
+  }
+}
