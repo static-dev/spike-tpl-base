dfef475661975cd0c1b134dc699a7acbd1b9e356
Jeff Escalante <hello@jenius.me>
2016-05-26 11:41:46 -0400

    fix babel presets issue

--- root/app.js
+++ root/app.js
@@ -2,13 +2,15 @@ const cssnext = require('postcss-cssnext')
 const rucksack = require('rucksack-css')
 const sugarss = require('sugarss')
 const lost = require('lost')
+const es2015 = require('babel-preset-es2015')
+const stage2 = require('babel-preset-stage-2')
 
 module.exports = {
   postcss: {
     plugins: [cssnext, rucksack, lost],
     parser: sugarss
   },
-  babel: { presets: ['es2015', 'stage-2'] },
+  babel: { presets: [es2015, stage2] },
   locals: { foo: 'bar' },
   ignore: ['**/layout.jade', '**/_*', '**/.*']
 }
