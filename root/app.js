3282e4830a5b1a9ae4e3fa4793bc290f2d09b981
Jeff Escalante <escalantejeff@gmail.com>
2017-02-10 14:38:55 -0500

    remove hard source plugin, causing things to break

--- root/app.js
+++ root/app.js
@@ -1,5 +1,3 @@
-const path = require('path')
-const HardSourcePlugin = require('hard-source-webpack-plugin')
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('babel-preset-env')
@@ -16,12 +14,5 @@ module.exports = {
     locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }
   }),
   postcss: cssStandards(),
-  babel: { presets: [[jsStandards, { modules: false }]] },
-  plugins: [
-    new HardSourcePlugin({
-      environmentPaths: { root: __dirname },
-      recordsPath: path.join(__dirname, '_cache/records.json'),
-      cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
-    })
-  ]
+  babel: { presets: [[jsStandards, { modules: false }]] }
 }
