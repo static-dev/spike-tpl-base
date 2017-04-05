e53234cf76192ae289e063f9242f58c41426fdbd
Jeff Escalante <jescalan@users.noreply.github.com>
2017-04-05 18:45:03 -0400

    Webpack2 conversion (#59)
    * start webpack2 conversion
    
    * more updates
    
    * update for next spike version
    
    * update babel plugin, re-add locals example
    
    * add note on babel preset
    
    * upgrade to babel preset env
    
    * remove hard source plugin, causing things to break
    
    * remove cache folder
    
    * Update production config file (#61)
    
    * Update production config to remove dedupeplugin
    * Include offline-plugin as dev-dep and require client runtime
    
    * prepare for spike v1 release

--- root/app.js
+++ root/app.js
@@ -1,8 +1,7 @@
-const path = require('path')
-const HardSourcePlugin = require('hard-source-webpack-plugin')
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
-const jsStandards = require('babel-preset-latest')
+const jsStandards = require('babel-preset-env')
+const dynamicImport = require('babel-plugin-syntax-dynamic-import')
 const pageId = require('spike-page-id')
 
 module.exports = {
@@ -11,22 +10,10 @@ module.exports = {
     html: '*(**/)*.sgr',
     css: '*(**/)*.sss'
   },
-  ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md'],
-  reshape: (ctx) => {
-    return htmlStandards({
-      webpack: ctx,
-      locals: { pageId: pageId(ctx), foo: 'bar' }
-    })
-  },
-  postcss: (ctx) => {
-    return cssStandards({ webpack: ctx })
-  },
-  babel: { presets: [jsStandards] },
-  plugins: [
-    new HardSourcePlugin({
-      environmentPaths: { root: __dirname },
-      recordsPath: path.join(__dirname, '_cache/records.json'),
-      cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
-    })
-  ]
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  reshape: htmlStandards({
+    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }
+  }),
+  postcss: cssStandards(),
+  babel: { presets: [[jsStandards, { modules: false }]], plugins: [dynamicImport] }
 }
--- root/app.production.js
+++ root/app.production.js
@@ -1,35 +1,16 @@
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
-const pageId = require('spike-page-id')
-const {UglifyJsPlugin, DedupePlugin, OccurrenceOrderPlugin} = require('webpack').optimize
+const {UglifyJsPlugin} = require('webpack').optimize
 
 module.exports = {
   // disable source maps
   devtool: false,
-  // webpack optimization and minfication plugins
-  plugins: [
-    new UglifyJsPlugin(),
-    new DedupePlugin(),
-    new OccurrenceOrderPlugin()
-  ],
-  // image optimization
-  module: {
-    loaders: [{ test: /\.(jpe?g|png|gif|svg)$/i, loader: 'image-webpack' }]
-  },
-  // adds html minification plugin
-  reshape: (ctx) => {
-    return htmlStandards({
-      webpack: ctx,
-      locals: { pageId: pageId(ctx), foo: 'bar' },
-      minify: true
-    })
-  },
-  // adds css minification plugin
-  postcss: (ctx) => {
-    return cssStandards({
-      webpack: ctx,
-      minify: true,
-      warnForDuplicates: false // cssnano includes autoprefixer
-    })
-  }
+  // minify js
+  plugins: [new UglifyJsPlugin()],
+  // minify html and css
+  reshape: htmlStandards({ minify: true }),
+  postcss: cssStandards({
+    minify: true,
+    warnForDuplicates: false // cssnano includes autoprefixer
+  })
 }
