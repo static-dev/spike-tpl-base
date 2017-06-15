811f70837053d6743bd00f55d1eb2fbb0c0816dd
Jeff Escalante <escalantejeff@gmail.com>
2017-06-15 13:02:41 -0400

    dependencies and small structure updates

--- root/app.js
+++ root/app.js
@@ -2,17 +2,19 @@ const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
+const env = process.NODE_ENV
 
 module.exports = {
   devtool: 'source-map',
-  matchers: {
-    html: '*(**/)*.sgr',
-    css: '*(**/)*.sss'
-  },
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }
+    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
+    minify: env === 'production'
   }),
-  postcss: cssStandards(),
+  postcss: cssStandards({
+    minify: env === 'production',
+    warnForDuplicates: env !== 'production'
+  }),
   babel: jsStandards()
 }
