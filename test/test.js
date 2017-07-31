2804fbce084a6c922c7a8ce64be3a67cb2324b5e
Jeff Escalante <escalantejeff@gmail.com>
2017-07-31 09:17:57 -0400

    fix tests

--- test/test.js
+++ test/test.js
@@ -22,7 +22,34 @@ test.skip('compile test', (t) => {
 
 test('initializes with sprout, compiles with spike', t => {
   const tplName = 'spike-tpl-base-test'
-  const locals = { name: 'doge', description: 'wow', github_username: 'amaze', production: false }
+  const locals = { name: 'doge', description: 'wow', github_username: 'amaze', production: false, sugar: false }
+  const sprout = new Sprout(tmpdir())
+
+  t.plan(1)
+
+  return sprout.add(tplName, path.resolve(__dirname, '..'))
+    .tap(() => console.log('initializing template...'))
+    .then(sprout.init.bind(sprout, tplName, tplTestPath, { locals: locals }))
+    .tap(() => console.log('installing dependencies...'))
+    .then(npmInstall.bind(null, tplTestPath))
+    .tap(() => console.log('compiling with spike...'))
+    .then(() => {
+      return W.promise((resolve, reject) => {
+        const project = new Spike({ root: tplTestPath })
+        project.on('error', reject)
+        project.on('compile', resolve)
+        project.compile()
+      })
+    })
+    .then(() => { t.is(true, true) })
+    .finally(() => {
+      return sprout.remove.bind(sprout, tplName)
+    })
+})
+
+test('compiles with sugar option', t => {
+  const tplName = 'spike-tpl-base-test'
+  const locals = { name: 'doge', description: 'wow', github_username: 'amaze', production: false, sugar: true }
   const sprout = new Sprout(tmpdir())
 
   t.plan(1)
@@ -49,7 +76,7 @@ test('initializes with sprout, compiles with spike', t => {
 
 test('compiles with production setting', t => {
   const tplName = 'spike-tpl-base-production-test'
-  const locals = { name: 'doge', description: 'wow', github_username: 'amaze', production: true }
+  const locals = { name: 'doge', description: 'wow', github_username: 'amaze', production: true, sugar: false }
   const sprout = new Sprout(tmpdir())
 
   t.plan(1)
